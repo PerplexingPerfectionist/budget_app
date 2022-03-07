474d1ead2803b75960783b56b749f47630a41b3f
Bobby McMillen <bobbym@virtualofficepro.com>
2022-03-07 14:33:10 -0600

    localStorage and all functions

--- src/contexts/BudgetsContext.js
+++ src/contexts/BudgetsContext.js
@@ -1,4 +1,6 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
+import { v4 as uuidV4 } from 'uuid'
+import useLocalStorage from '../hooks/useLocalStorage'
 
 const BudgetsContext = React.createContext()
 
@@ -7,5 +9,53 @@ export function useBudgets() {
 }
 
 export const BudgetsProvider = ({ children }) => {
-    return children
+    const [budgets, setBudgets] = useLocalStorage('budgets', [])
+    const [expenses, setExpenses] = useLocalStorage('expenses', [])
+
+    function getBudgetExpenses(budgetId) {
+        //Filter expenses by ID
+        return expenses.filter(expense => expense.budgetId === budgetId)
+    }
+
+    function addExpense({ description, amount, budgetId }) {
+        setExpenses(prevExpenses => {
+            return [...prevExpenses, { id: uuidV4(), description, amount, budgetId }]
+        })
+    }
+
+    function addBudget({ name, max }) {
+        setBudgets(prevBudgets => {
+            //Check if Budget exists
+            if (prevBudgets.find(budget => budget.name === name)) {
+                return prevBudgets
+            }
+            return [...prevBudgets, { id: uuidV4(), name, max }]
+        })
+    }
+
+    function deleteExpense({ id }) {
+        //Filter out expense by id to remove
+        setExpenses(prevExpenses => {
+            return prevExpenses.filter(expense => expense.id !== id)
+        })
+    }
+
+    function deleteBudget({ id }) {
+        //Filter out budget by id to remove
+        setBudgets(prevBudgets => {
+            return prevBudgets.filter(budget => budget.id !== id)
+        })
+    }
+    
+    return (
+        <BudgetsContext.Provider value={{
+            budgets,
+            expenses,
+            getBudgetExpenses,
+            addExpense,
+            addBudget,
+            deleteExpense,
+            deleteBudget
+        }}>{children}</BudgetsContext.Provider>
+    )
 }
---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react"
+
+export default function useLocalStorage(key, defaultValue) {
+    const [value, setValue] = useState(() => {
+        const jsonValue = localStorage.getItem(key)
+        //If something is stored, return it
+        if (jsonValue != null) return JSON.parse(jsonValue(jsonValue))
+
+        if (typeof defaultValue === 'function') {
+            return defaultValue()
+        } else {
+            return defaultValue
+        }
+    })
+
+    //Update localStorage if values changes
+    useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(value))
+    }, [key, value])
+
+    return [value, setValue]
+}
