e90b5185d01abe52412ac5549e38f449217e1db3
Bobby McMillen <bobbym@virtualofficepro.com>
2025-12-12 08:10:50 -0600

    Migrate to Vite

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BudgetsProvider } from "./contexts/BudgetsContext";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BudgetsProvider>
+      <App />
+    </BudgetsProvider>
+  </StrictMode>
+);
